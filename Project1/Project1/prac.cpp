a62189dc124a5eed3998ea67b2deb42b95d1a882
park020311 <zxwrtygsnm@naver.com>
2024-02-23 18:39:11 +0900

    prac class

--- Project1/Project1/prac.cpp
+++ Project1/Project1/prac.cpp
@@ -1,9 +1,54 @@
 #include<iostream>
+#include<string>
 using namespace std;
 
-int main() {
+class Character {
+private:
+	string name;
+	int level;
+public:
+	Character(string recName, int recLevel) {
+		name = recName;
+		level = recLevel;
 
-	cout << "hello world";
+
+	}
+	void printInformation() {
+		cout << "아이디 : " << name << endl;
+		cout << "레벨 : " << level << endl;
+		
+	}
+
+	void setLevel(int changedLevel) {
+		level = changedLevel;
+	}
+	int getLevel() {
+		return level;
+	}
+	string getName() {
+		return name;
+	}
+
+};
+
+
+
+int main() {
+	string tmpName;
+	cin >> tmpName;
+
+	int tmpLevel;
+	cin >> tmpLevel;
 	
+	Character p1(tmpName, tmpLevel);
+	p1.printInformation();
+
+	p1.setLevel(p1.getLevel() + 1);
+	p1.printInformation();
+
+	
+
+
+
 	return 0;
 }
