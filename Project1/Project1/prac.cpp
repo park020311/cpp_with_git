342ac8becbe3db32c033a9754de4b58a45381279
park020311 <zxwrtygsnm@naver.com>
2024-02-26 18:06:49 +0900

    static practice

--- Project1/Project1/prac.cpp
+++ Project1/Project1/prac.cpp
@@ -1,37 +1,36 @@
 #include<iostream>
 using namespace std;
 
-class Test {
+class Player {
 private:
-	int num;
+	string name;
 public:
-	Test(int n) {
-		num = n;
-		cout << num << "생성자 실행" << endl;
-
+	static int playerCount; // 정적 멤버 변수 선언 (Player라는 클래스를 가지고 만든 
+	Player(string _name) {	// 모든 객체들이 공유하는 변수
+		name = _name;
+		playerCount++;
 	}
-	~Test() {
-		cout << num << "소멸자 실행" << endl;
+	~Player() {
+		playerCount--;
 	}
-	void printData() {
-		cout << "가지고 있는 값 : " << num << endl;
+	void printName() {
+		cout << "이름 : " << name << endl;
 	}
 };
+int Player::playerCount = 0; // 정적 멤버 변수의 초기화
 
-void function() {
-
-	Test t3(3);
-	Test t4(4);
-}
 int main() {
-	Test t1(1);
-	Test t2(2);
-
-	function();
-
-	Test t5(5);
-	Test t6(6);
-
 	
+	Player p1("인덕이");
+	
+	Player p2("언덕이");
+
+	Player p3("인경이");
+
+	cout << "플레이어 수 : " << Player::playerCount << endl;
+	
+	
+
+
 	return 0;
 }
